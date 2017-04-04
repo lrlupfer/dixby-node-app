78957d67d39f69053ebf05969ed5fd7a76d1fac5
lindsey <lrlupfer@gmail.com>
2017-04-04 16:01:52 -0400

    initial files for homework

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'FhgIKoecoXkOvqB5i5pSKFa2S',
+  consumer_secret: 'Z6N53iuuQhKionS8QAZINPEKZF3GnuiHuEVRB0ITz2mfcHnXV5',
+  access_token_key: '19952755-d8uG3jLFnxC6eAXjXfzVxkcxt3MtZYVRUCepzVhll',
+  access_token_secret: 'A8hT8WO8aSs19Fu0U5arkruGe7guw9DHl936JjjphjfZA',
+}
